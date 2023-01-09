c53c62bb3c068a77f7f0cdebae114fa9c6968fa7
Luiz Ricardo Lourenço da Silva <46008216+R-Lourenco1998@users.noreply.github.com>
2023-01-09 16:46:02 -0300

    antes do springboot 3.0

--- crud-spring/src/main/java/com/ricardo/crudspring/model/Course.java
+++ crud-spring/src/main/java/com/ricardo/crudspring/model/Course.java
@@ -1,12 +1,20 @@
 package com.ricardo.crudspring.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Data
 @Entity
 //@Table(name = "cursos")
+@SQLDelete(sql = "UPDATE Course SET status 'Inativo' WHERE id = ?")
+@Where(clause = "status = 'Ativo'")
 public class Course {
 
     @Id
@@ -14,10 +22,22 @@ public class Course {
     @JsonProperty("_id")
     private Long id;
 
-    @Column(name = "nome" ,length = 200, nullable = false)
+    @NotBlank
+    @NotNull
+    @Length(min = 5, max = 100)
+    @Column(name = "nome" ,length = 100, nullable = false)
     private String name;
 
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Back-end|Front-end")
     @Column(name = "categoria", length = 10, nullable = false)
     private String category;
+
+    @NotNull
+    @Length(max = 100)
+    @Pattern(regexp = "Ativo|Inativo")
+    @Column(name = "categoria", length = 10, nullable = false)
+    private String status = "Ativo";
 
 }
